c3bfe65f5cb95e818094de524a135b996d20f880
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-07-04 19:59:54 +0300

    add src\config.py

---
+++ src/config.py
@@ -0,0 +1,27 @@
+import json
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
+
+
+class Settings:
+    DB_NAME = 'cw5'
+    DB_USER = os.environ['DB_NAME']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+    DB_HOST = os.environ['DB_HOST']
+    DB_PORT = os.environ['DB_PORT']
+    EMPLOYEE_IDS_CONFIG = BASE_DIR.joinpath('ten_employers.json')
+    MIGRATIONS_DIR = BASE_DIR.joinpath('src', 'migrations')
+
+    def get_employer_ids(self) -> list[int]:
+        with self.EMPLOYEE_IDS_CONFIG.open() as f:
+            data = json.load(f)
+
+            return data['employers']['hh']
+
+
+settings = Settings()
+
