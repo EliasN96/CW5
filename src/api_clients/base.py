e6aa5ceeef57619bf777a0588dba0b3e85418eab
Ilya_Novikov <119965367+EliasN96@users.noreply.github.com>
2024-07-06 13:57:38 +0300

    Update base.py

--- src/api_clients/base.py
+++ src/api_clients/base.py
@@ -3,13 +3,22 @@ import requests
 
 
 class APIClient(ABC):
+    """
+    Абстрактный класс для работы с API
+    """
 
     @property
     @abstractmethod
     def base_url(self) -> str:
+        """
+        Абстрактный метод получения ссылки на hh.ru
+        """
         pass
 
     def _get(self, url: str, params: dict = {}) -> dict:
+        """
+        Метод получения информации возвращающий словарь
+        """
         full_url = self.base_url + url
 
         response = requests.get(full_url, params=params, timeout=5)
