84bbdfc05fd735d29f836381a5b2e1395cbabef3
Ilya Novikov <illianovikovnitzana@gmail.com>
2024-07-06 16:22:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.user_interaction import run_interaction
+from src.cli import run
 
 
 def main():
     """
     Главный метод программы, который её запускает
     """
+    run()
     run_interaction()
 
 
